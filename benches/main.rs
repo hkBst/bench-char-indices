6fb6c295cb776817c6d63d146f7afd70644901cf
Marijn Schouten <mhkbst@gmail.com>
2025-06-05 18:15:31 +0000

    add benches for mixed ascii and non-ascii strings, and check entire output

--- benches/main.rs
+++ benches/main.rs
@@ -9,15 +9,17 @@ const LEN: usize = 10_000;
 
 macro_rules! fn_bench_check_raw {
     ($name:ident, $unit:ty, $check_raw:ident) => {
-        fn $name(b: &mut test::Bencher, s: &str, expected: $unit) {
+        fn $name(b: &mut test::Bencher, s: &str, expected: Vec<$unit>) {
             let input: String = test::black_box(repeat_n(s, LEN).collect());
             assert_eq!(input.len(), LEN * s.len());
             b.iter(|| {
                 let mut output = vec![];
 
                 $check_raw(&input, |range, res| output.push((range, res)));
-                assert_eq!(output.len(), LEN);
-                assert_eq!(output[0], ((0..s.len()), Ok(expected)));
+                assert_eq!(output.len(), LEN * s.chars().count());
+                for ((i, &e), (p, c)) in expected.iter().enumerate().zip(s.char_indices()) {
+                    assert_eq!(output[i], ((p..p + c.len_utf8()), Ok(e)));
+                }
             });
         }
     };
@@ -32,10 +34,42 @@ fn_bench_check_raw!(
 
 #[bench]
 fn bench_check_raw_str_ascii_while(b: &mut test::Bencher) {
-    bench_check_raw_str_while(b, "a", 'a');
+    bench_check_raw_str_while(b, "a", repeat_n('a', LEN).collect());
 }
 
 #[bench]
 fn bench_check_raw_str_ascii_char_indices(b: &mut test::Bencher) {
-    bench_check_raw_str_char_indices(b, "a", 'a');
+    bench_check_raw_str_char_indices(b, "a", repeat_n('a', LEN).collect());
+}
+
+#[bench]
+fn bench_check_raw_str_unicode_while(b: &mut test::Bencher) {
+    bench_check_raw_str_while(b, "🦀", repeat_n('🦀', LEN).collect());
+}
+
+#[bench]
+fn bench_check_raw_str_unicode_char_indices(b: &mut test::Bencher) {
+    bench_check_raw_str_char_indices(b, "🦀", repeat_n('🦀', LEN).collect());
+}
+
+#[bench]
+fn bench_check_raw_str_mixed_while(b: &mut test::Bencher) {
+    bench_check_raw_str_while(
+        b,
+        "a🦀",
+        (0..2 * LEN)
+            .map(|i| if i % 2 == 0 { 'a' } else { '🦀' })
+            .collect(),
+    );
+}
+
+#[bench]
+fn bench_check_raw_str_mixed_char_indices(b: &mut test::Bencher) {
+    bench_check_raw_str_char_indices(
+        b,
+        "a🦀",
+        (0..2 * LEN)
+            .map(|i| if i % 2 == 0 { 'a' } else { '🦀' })
+            .collect(),
+    );
 }
